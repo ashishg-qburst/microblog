e259da217239ea6b9e6a31b159225c8c2c94bc3d
ash <ashishg@qburst.com>
2016-07-07 17:47:32 +0530

    Add signup facility

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+  #Returns Gravatar of user
+  def gravatar_for(user)
+    g_id = Digest::MD5::hexdigest(user.email.downcase)
+    g_url = "https://secure.gravatar.com/avatar/#{g_id}"
+    image_tag(g_url, alt: user.name, class: "gravatar")
+  end
 end
