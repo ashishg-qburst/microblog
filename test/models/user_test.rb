500cc24a455960796fd42a44b63be18cdce6a74e
ash <ashishg@qburst.com>
2016-07-07 11:49:29 +0530

    Add secure password to user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+      password: "foobaz", password_confirmation: "foobaz")
   end
 
   test "should be valid" do
@@ -38,19 +39,24 @@ class UserTest < ActiveSupport::TestCase
       end
     end
 
-    test "email validation should reject invalid addresses" do
-      invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-        foo@bar_baz.com foo@bar+baz.com]
-        invalid_addresses.each do |invalid_address|
-          @user.email = invalid_address
-          assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
-        end
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+      foo@bar_baz.com foo@bar+baz.com]
+      invalid_addresses.each do |invalid_address|
+        @user.email = invalid_address
+        assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
       end
+    end
 
-      test "email addresses should be unique" do
-        duplicate_user = @user.dup
-        duplicate_user.email = @user.email.upcase
-        @user.save
-        assert_not duplicate_user.valid?
-      end
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
 end
