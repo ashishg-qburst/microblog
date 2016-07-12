dd090b1d0c99d35ebd2bc23a762e6fc27d9d7004
ash <ashishg@qburst.com>
2016-07-12 09:56:24 +0530

    Finish delete user and add admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def show
     @user = User.find(params[:id])
@@ -40,10 +41,16 @@ class UsersController < ApplicationController
     @users = User.page(params[:page]).per(10)
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User Deleted."
+    redirect_to users_url
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password,
-        :password_confirmation)
+                                          :password_confirmation)
     end
 
     def logged_in_user
@@ -57,5 +64,9 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to root_url unless current_user?(@user) 
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
