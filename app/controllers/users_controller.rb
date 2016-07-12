7d7355f5fb14cfcc781cf26aa94b1e54cf1b3eb3
ash <ashishg@qburst.com>
2016-07-12 14:33:41 +0530

    Add account activations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to MicroBlog"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
@@ -48,25 +48,25 @@ class UsersController < ApplicationController
   end
 
   private
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                          :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+      :password_confirmation)
+  end
 
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Not logged in."
-        redirect_to login_url
-      end
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Not logged in."
+      redirect_to login_url
     end
+  end
 
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to root_url unless current_user?(@user) 
-    end
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_url unless current_user?(@user) 
+  end
 
-    def admin_user
-      redirect_to(root_url) unless current_user.admin?
-    end
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
