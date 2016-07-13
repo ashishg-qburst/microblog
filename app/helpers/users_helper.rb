82dfe2d01f6432699d14e34227db2a47720c050c
ash <ashishg@qburst.com>
2016-07-13 12:53:46 +0530

    Add micropost model and mcpost views

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,9 @@
 module UsersHelper
   #Returns Gravatar of user
-  def gravatar_for(user)
+  def gravatar_for(user, options = { size: 50 })
     g_id = Digest::MD5::hexdigest(user.email.downcase)
-    g_url = "https://secure.gravatar.com/avatar/#{g_id}"
+    size = options[:size]
+    g_url = "https://secure.gravatar.com/avatar/#{g_id}?s=#{size}"
     image_tag(g_url, alt: user.name, class: "gravatar")
   end
 end
