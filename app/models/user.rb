d9aa96ded0167e7502d07306f22585d8ec80168a
ash <ashishg@qburst.com>
2016-07-14 10:46:30 +0530

    Create Relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
+
+  has_many :active_relationships, class_name: "Relationship",
+  foreign_key: "follower_id",
+  dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+
+  has_many :passive_relationships, class_name: "Relationship",
+  foreign_key: "followed_id",
+  dependent: :destroy
+  has_many :followers, through: :passive_relationships
+
   attr_accessor :remember_token, :activation_token, :reset_token
   before_create :create_activation_token
   before_save { self.email = email.downcase }
@@ -10,6 +21,18 @@ class User < ActiveRecord::Base
   format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }, allow_blank: true
+
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+  
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+  
+  def following?(other_user)
+    following.include?(other_user)
+  end
 
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
