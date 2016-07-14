bec348c6ba0a362d2cb255ffb070f3263fe765fa
ash <ashishg@qburst.com>
2016-07-14 12:36:26 +0530

    Add followed user posts to feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,7 +39,8 @@ class User < ActiveRecord::Base
   end
 
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
   end
 
   def User.digest(string)
